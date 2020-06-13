8d1d18d0fe61ddd63f818653493dd0cf96b2a837
scardenas <scardenas@cidenet.com.co>
2020-06-12 20:24:19 -0500

    feat(templateDriven): template driven form added

---
+++ Reactive forms/README.txt
@@ -0,0 +1,13 @@
+TEMPLATE-DRIVEN FORMS
+        - Generated form model
+	- HTML validation
+        - Two-way data binding
+	- Use formModule
+	- asynchronous
+	- Most of the logic is driven in the template
+
+REACTIVE FORMS
+        - Manually created form model
+	- Use ReactiveFormModule
+	- synchronous
+	- Logic resides in the component or the manin typescript code
---
+++ Reactive forms/src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../Models/user';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-driven-form',
+  templateUrl: './template-driven-form.component.html',
+  styleUrls: ['./template-driven-form.component.css']
+})
+export class TemplateDrivenFormComponent implements OnInit {
+  user = new User();
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  save(userForm: NgForm){
+    console.log(userForm.form)
+    console.log('Guardado: ' +JSON.stringify(userForm.value))
+    
+  }
+
+}
